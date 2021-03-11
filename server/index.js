8af0268e9d620c71591f3ae6ba353487fba5bd31
dissantos <disantosg18@gmail.com>
2021-03-11 10:33:54 -0300

    Access Heroku PostgreSQL database

--- server/index.js
+++ server/index.js
@@ -2,48 +2,47 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
-const { Client } = require('pg')
+const { Pool } = require('pg')
+const { Router } = require('express')
 
-client = new Client({
+pool = new Pool({
     host: 'ec2-52-44-31-100.compute-1.amazonaws.com',
     port: 5432,
+    database: 'd1ls3adtuirs23',
     user: 'iybbjmnbkukkwy',
-    password: 'aecdd5d7c9aea03866476fb1af9f92d1e299d1bc5dc1f2440bf7064f27fc8a02'
+    password: 'aecdd5d7c9aea03866476fb1af9f92d1e299d1bc5dc1f2440bf7064f27fc8a02',
+    ssl: { rejectUnauthorized: false }
 })
 
-try{
-client
-  .connect()
-  console.log('Conectado.')
-} catch (error){
-    console.log('Não foi possível conectar.')
-}
 
-app.use(require("cors")()) 
-app.use(bodyParser.json()) 
+
+app.use(require("cors")())
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
 app.use('/', router)
 
-client.query('SELECT * FROM pessoa', (err, res) => {
-    if (err){
-        console.log('Erro!!!')
-    }
-    console.log(JSON.stringify(res))
-    client.end()
-  })
-  
+
+
+
+const getPessoa = (req, resp) => {
+    //SELECT * FROM pessoa
+    pool.query('SELECT * FROM pessoa', (err, res) => {
+        if (err) {
+            console.log('Erro!!!')
+        }
+        resp.status(200).json(res.rows)
+        pool.end();
+    })
+}
+
+
 router
     .route('/')
-    .get(async(req,res) => {
-        let x = await client.query('SELECT * FROM pessoa', (err, res) => {
-            if (err){
-                console.log('Erro!!!')
-            }
-            console.log(JSON.stringify(res))
-            client.end()
-          })
-        console.log(x)
-        res.send(x)
-    })
+    .get(getPessoa)
 
 // listen for requests on port 8000
 const port = 8000
