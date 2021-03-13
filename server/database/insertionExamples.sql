d7d1f2870cb2651fd1ec4996721b591b3732168b
Renan Siman <31517127+RenanSiman@users.noreply.github.com>
2021-03-12 23:55:04 -0300

    fix dbPessoa

---
+++ server/database/insertionExamples.sql
@@ -0,0 +1,17 @@
+INSERT INTO pessoa (nome , email , telefone , cep , logradouro , bairro , cidade , estado)
+VALUES ('Renan', 'renan@email.com', '31988886666','3011012','Rua 1', 'Floresta', 'BH', 'MG');
+
+INSERT INTO funcionario (datacontrato, salario, senha, status)
+VALUES ('10-02-2015', 4507.52, '0123', true);
+
+INSERT INTO paciente (peso, altura, tiposanguineo)
+VALUES (80.5, 1.81, 'O-');
+
+INSERT INTO medico (especialidade, crm)
+VALUES ('Neurocirurgião', 4567518);
+
+INSERT INTO base_enderecos (cep, logradouro, bairro, cidade, estado)
+VALUES ('3011012','Rua 1', 'Floresta', 'BH', 'MG');
+
+INSERT INTO agenda (data, horario, nome, email, telefone)
+VALUES ('25-04-2021', '25-04-2021T16:00:00', 'Roberto', 'roberto@email.com','38976543210');
