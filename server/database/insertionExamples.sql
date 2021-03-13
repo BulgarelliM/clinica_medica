5d2c3fadf35dafe5374f093a7777176f1086d069
bulgarellim <bulgarelli.mariana1@gmail.com>
2021-03-13 19:15:33 -0300

    examples

--- server/database/insertionExamples.sql
+++ server/database/insertionExamples.sql
@@ -15,3 +15,6 @@ VALUES ('3011012','Rua 1', 'Floresta', 'BH', 'MG');
 
 INSERT INTO agenda (data, horario, nome, email, telefone)
 VALUES ('25-04-2021', '25-04-2021T16:00:00', 'Roberto', 'roberto@email.com','38976543210');
+
+insert into agenda (idAgenda, data, horario, nome, email, telefone, medico_id) 
+values ('a1', '01-04-2021', '14:00:00', 'Paulo', 'paulo@email.com', '33985555', 'm01');
