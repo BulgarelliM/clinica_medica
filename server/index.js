40f19cf8a603fbccb313af9753149c8bce00ec10
bulgarellim <bulgarelli.mariana1@gmail.com>
2021-03-12 21:40:44 -0300

    change routes

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ const bodyParser = require('body-parser')
 const app = express()
 const router = express.Router()
 const dbPessoa = require('./database/dbPessoa')
+const dbPaciente = require('./database/dbPaciente')
+const dbMedico = require('./database/dbMedico')
+const dbFuncionario = require('./database/dbFuncionario')
+const dbBaseEndereco = require('./database/dbBaseEndereco')
+const dbAgenda = require('./database/dbAgenda')
 
 app.use(require("cors")())
 app.use(bodyParser.json())
@@ -10,16 +15,45 @@ app.use(
     bodyParser.urlencoded({
         extended: true,
     })
-    )
+)
 
 app.use('/', router)
 
 router
-    .route('/')
+    .route('/pessoa')
     .get(dbPessoa.getPessoa)
-    //.get(dbPessoa.getPessoaId)
     .post(dbPessoa.setPessoa)
     .put(dbPessoa.atualizaPessoa)
+
+router
+    .route('/paciente')
+    .get(dbPaciente.getPaciente)
+    .post(dbPaciente.setPaciente)
+    .put(dbPaciente.atualizaPaciente)
+
+router
+    .route('/medico')
+    .get(dbMedico.getMedico)
+    .post(dbMedico.setMedico)
+    .put(dbMedico.atualizaMedico)
+
+router
+    .route('/funcionario')
+    .get(dbFuncionario.getFuncionario)
+    .post(dbFuncionario.setFuncionario)
+    .put(dbFuncionario.atualizaFuncionario)
+
+router
+    .route('/endereco')
+    .get(dbBaseEndereco.getEndereco)
+    .post(dbBaseEndereco.setEndereco)
+    .put(dbBaseEndereco.atualizaEndereco)
+
+router
+    .route('/agenda')
+    .get(dbAgenda.getAgenda)
+    .post(dbAgenda.setAgenda)
+    .put(dbAgenda.atualizaAgenda)
 
 // listen for requests on port 8000
 const port = 8000
